7c1775056487332c40a59e9bdf66a3cd21845aa7
klabarre <klabarre@dawan.fr>
2024-09-04 09:41:44 +0200

    push

---
+++ src/view/ComposantClass.jsx
@@ -0,0 +1,54 @@
+import { Component } from "react";
+
+export class ClassComponent extends Component {
+  // Le constructeur est appelé lorsqu'une instance de la classe est créée.
+  constructor(props) {
+    // Appel au constructeur parent avec les props
+    super(props);
+    // Initialisation de l'état local count à 0
+    this.state = {
+      count: 0,
+    };
+    // Lier la méthode incrementCount à l'instance actuelle de la classe
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  // componentDidMount est appelé après que le composant est monté dans le DOM.
+  componentDidMount() {
+    console.log("Class Component mounted");
+  }
+
+  // componentWillUnmount est appelé juste avant que le composant soit retiré du DOM.
+  componentWillUnmount() {
+    console.log("Class Component unmounted");
+  }
+
+  // Méthode pour incrémenter le compteur
+  incrementCount() {
+    // Utilisation de setState pour mettre à jour l'état local count
+    // prevState représente l'état précédent du composant
+
+    console.log("Class Component updated");
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  }
+
+  // La méthode render est obligatoire et retourne le JSX à afficher.
+  render() {
+    return (
+      <div>
+        <h1>Class Component</h1>
+        {/* Affichage du compteur et bouton pour incrémenter */}
+        <p>Je suis le composant class !</p>
+
+        <div className="flex w-fit border p-2 m-5">
+          <p className="pr-5">Count: {this.state.count}</p>
+          <button className="btn btn-primary" onClick={this.incrementCount}>
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/view/CycleDeVie.jsx
+++ src/view/CycleDeVie.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 export const CycleDeVie = () => {
+  const [count, setCount] = useState(0);
+
+  console.log("test");
+
+  useEffect(() => {
+    // Sera exécuté au montage du composant
+    // SI je laisse le tableau de dépendance vie, sera exécuté UNIQUEMENT au montage du composant
+    console.log("composant monté");
+
+    return () => {
+      console.log("return du useEffect (exécuté au démontage du composant)");
+    };
+  }, []);
+
   return (
     <div className="pt-20">
       <h1>Cycle de vie</h1>
+      <p>count: {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Incrémenter</button>
+      <p>Composant class: </p>
     </div>
   );
 };
