11e79f16462d61347bfc4e7507ca9e69566be710
klabarre <klabarre@dawan.fr>
2024-09-05 10:19:48 +0200

    dernier push

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,83 @@
-/* eslint-disable react/no-unescaped-entities */
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import JsxTheorie from "./view/JsxTheorie";
+import HookUseState from "./view/HookUseState";
 import { Navbar } from "./components/Navbar";
+import { Formulaire } from "./view/Formulaire";
+import { Flux } from "./view/Flux";
+import { Exercice } from "./view/Exercice";
+import { HookUseEffect } from "./view/HookUseEffect";
+import { CycleDeVie } from "./view/CycleDeVie";
+import { ComposantClass } from "./view/ComposantClass";
+import { RequeteApi } from "./view/RequeteApi";
+import { ExerciceApi } from "./view/ExerciceApi";
+import { HookPersonnalise } from "./view/HookPersonnalise";
+import { HookUseMemo } from "./view/HookUseMemo";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <>Attention, élément non chargé</>,
-      children: [
-        {
-          path: "",
-          element: <>Page d'accueil</>,
-        },
-        {
-          path: "/jsx",
-          element: <JsxTheorie />,
-        },
-        {
-          path: "*",
-          element: <>Erreur 404</>,
-        },
-      ],
-    },
-  ]);
-
-  function Root() {
-    return (
-      <>
-        <Navbar />
-        <Outlet />;
-      </>
-    );
-  }
-
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<JsxTheorie />} />
+          <Route path="hook-use-state" element={<HookUseState />} />
+          <Route path="formulaire" element={<Formulaire />} />
+          <Route path="flux" element={<Flux />} />
+          <Route path="exercice" element={<Exercice />} />
+          <Route path="use-effect" element={<HookUseEffect />} />
+          <Route path="cycle-de-vie" element={<CycleDeVie />} />
+          <Route path="cycle-de-vie/class" element={<ComposantClass />} />
+          <Route path="requete-api" element={<RequeteApi />} />
+          <Route path="exercice-api" element={<ExerciceApi />} />
+          <Route path="hook-perso" element={<HookPersonnalise />} />
+          <Route path="hook-use-memo" element={<HookUseMemo />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
 
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./App.css";
+// import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 // import JsxTheorie from "./view/JsxTheorie";
-// import HookUseState from "./view/HookUseState";
 // import { Navbar } from "./components/Navbar";
-// import { Formulaire } from "./view/Formulaire";
-// import { Flux } from "./view/Flux";
-// import { Exercice } from "./view/Exercice";
-// import { HookUseEffect } from "./view/HookUseEffect";
-// import { CycleDeVie } from "./view/CycleDeVie";
-// import { ComposantClass } from "./view/ComposantClass";
-// import { RequeteApi } from "./view/RequeteApi";
-// import { ExerciceApi } from "./view/ExerciceApi";
-// import { HookPersonnalise } from "./view/HookPersonnalise";
-// import { HookUseMemo } from "./view/HookUseMemo";
-
 // function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Root />,
+//       errorElement: <>Attention, élément non chargé</>,
+//       children: [
+//         {
+//           path: "",
+//           element: <>Page d'accueil</>,
+//         },
+//         {
+//           path: "/jsx",
+//           element: <JsxTheorie />,
+//         },
+//         {
+//           path: "*",
+//           element: <>Erreur 404</>,
+//         },
+//       ],
+//     },
+//   ]);
+
+//   function Root() {
+//     return (
+//       <>
+//         <Navbar />
+//         <Outlet />;
+//       </>
+//     );
+//   }
+
 //   return (
 //     <>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<JsxTheorie />} />
-//           <Route path="hook-use-state" element={<HookUseState />} />
-//           <Route path="formulaire" element={<Formulaire />} />
-//           <Route path="flux" element={<Flux />} />
-//           <Route path="exercice" element={<Exercice />} />
-//           <Route path="use-effect" element={<HookUseEffect />} />
-//           <Route path="cycle-de-vie" element={<CycleDeVie />} />
-//           <Route path="cycle-de-vie/class" element={<ComposantClass />} />
-//           <Route path="requete-api" element={<RequeteApi />} />
-//           <Route path="exercice-api" element={<ExerciceApi />} />
-//           <Route path="hook-perso" element={<HookPersonnalise />} />
-//           <Route path="hook-use-memo" element={<HookUseMemo />} />
-//         </Routes>
-//       </BrowserRouter>
+//       <RouterProvider router={router} />
 //     </>
 //   );
 // }
